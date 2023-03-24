d625d204f80b64f235b14888b916a063b194269f
Arvid Hossain <arvidh97@gmail.com>
2023-03-24 14:29:58 -0400

    attempted #parent=

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,17 @@
 class PolyTreeNode
   
+    attr_reader :parent, :value, :children
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)
+        if @parent != node
+           @parent = node 
+        end
+    end
+
+
 end
